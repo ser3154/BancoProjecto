cd6ddc640c95fb09c49a54de2d52f6c8af72759d
ser3154 <159857682+ser3154@users.noreply.github.com>
2024-02-13 14:59:38 -0700

    Add files via upload

---
+++ Banco/src/main/java/Conexion/Conexion.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Serva
+ */
+public class Conexion implements IConexion {
+
+    final String cadenaConexion;
+    final String usuario;
+    final String contra;
+    private static final Logger LOG = Logger.getLogger(Conexion.class.getName());
+
+    public Conexion(String cadenaConexion, String usuario, String contra) {
+        this.cadenaConexion = cadenaConexion;
+        this.usuario = usuario;
+        this.contra = contra;
+    }
+
+    @Override
+    public Connection crearConexion() throws SQLException {
+        Connection conexion = DriverManager.getConnection(cadenaConexion, usuario, contra);
+        LOG.log(Level.INFO, "Conexion Establecida {0}", cadenaConexion);
+        return conexion;
+
+    }
+}
